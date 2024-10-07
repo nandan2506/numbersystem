ac021107ff57097cd618a0e8ac3efe8fee6feac7
Nandan Singh Danu <thenandansingh1999@gmail.com>
2024-10-07 12:56:10 +0530

    even and odd in the array

---
+++ index.js
@@ -0,0 +1,9 @@
+let arr=[3,7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,]
+for(let i=0; i<arr.length; i++)
+{
+    if (arr[i]%2==0)
+        console.log("even")
+    else
+    console.log("odd")
+}
+
